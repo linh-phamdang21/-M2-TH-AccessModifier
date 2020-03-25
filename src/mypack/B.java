16cc1a14748cf6d44f417c58cc4358bf75b2934e
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-03-25 15:29:19 +0700

    Add files

---
+++ src/mypack/B.java
@@ -0,0 +1,12 @@
+package mypack;
+
+import org.w3c.dom.ls.LSOutput;
+import pack.*;
+
+public class B extends A {
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.data);
+        obj.msg();
+    }
+}
---
+++ src/pack/A.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class A {
+    protected int data = 40;
+    protected void msg (){
+        System.out.println("Hello java");
+    }
+}
